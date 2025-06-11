74f7f4a4bb1f5a29a4da6d2f885ef9eb862fde99
damien-martinez-hub <darkstoll97414@gmail.com>
2025-06-11 11:39:52 +0200

    Update profil2.tsx

--- src/profil2.tsx
+++ src/profil2.tsx
@@ -175,7 +175,7 @@ const [state, setState] = useState<AppState>(() => {
                           
 
                       <div className="chat-response flex items-start gap-2.5">
-              <img className="w-8 h-8 rounded-full" src={img_logo} />
+              <img className="w-8 h-8 rounded-full" src={img_logo} alt="Jese"/>
               <div className="flex flex-col w-full max-w-[320px] leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
                   <div className="flex items-center space-x-2 rtl:space-x-reverse">
                     <span className="text-sm font-semibold text-gray-900 dark:text-white">BotAI</span>
